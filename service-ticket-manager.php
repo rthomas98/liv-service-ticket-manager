06ab3556bb60c56d3173177c58971f61c28f02fd
Rob Thomas <robt84@gmail.com>
2024-02-08 20:11:34 -0600

    Create service-ticket-manager.php

---
+++ service-ticket-manager.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: LIV Transport Service Ticket Manager
+Plugin URI:  https://www.empuls3.com/plugins/service-ticket-manager
+Description: A custom plugin to manage service tickets and integrate with QuickBooks
+Version:     1.0.0
+Author:      Empuls3
+Author URI:  https://www.empuls3.com
+License:     GPLv2 or later
+Text Domain: service-ticket-manager
+*/
